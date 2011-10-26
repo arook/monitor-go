f02350553da25e161eee7a2d3a8cab93e961b5ea
arook <arook@vip.qq.com>
2011-10-26 17:49:48 +0800

    store

--- store.go
+++ store.go
@@ -3,9 +3,42 @@ package main
 import (
   //"gob"
   "os"
+  "sync"
+  "log"
+)
+
+const (
+  saveTimeout = 10e9
+  saveQueueLength = 1000
 )
 
 type Store interface {
   Put(url, key *string) os.Error
   Get(key, url *string) os.Error
+}
+
+type MonitorStore struct {
+  mu sync.RWMutex
+  count int
+  save chan record
+}
+
+type record struct {
+  Key, URL string
+}
+
+func NewMonitorStore(filename string) *MonitorStore {
+  s := &MonitorStore{}
+  if filename != "" {
+    s.save = make(chan record, saveQueueLength)
+    if err := s.load(filename); err != nil {
+      log.Println("URLStore:", err)
+    }
+    //go s.saveLoop(filename)
+  }
+  return s
+}
+
+func (s *MonitorStore) load(filename string) os.Error {
+  return nil
 }
